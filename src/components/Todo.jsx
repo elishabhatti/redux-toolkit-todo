1a6e9d90995bd969c3ec062a78acf0e3f619e772
elisha <elishajameel2007@gmail.com>
2025-07-20 14:20:15 +0500

    added update icon in todos

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { removeTodo } from "../features/todo/todoSlice";
-import { X } from "lucide-react";
+import { Edit, X } from "lucide-react";
 import { motion, AnimatePresence } from "framer-motion";
 
 function Todo() {
@@ -22,13 +22,22 @@ function Todo() {
               className="bg-gray-900 rounded-lg px-4 py-3 flex justify-between items-center shadow-md border border-gray-700"
             >
               <span className="text-white text-base">{todo.text}</span>
-              <button
-                onClick={() => dispatch(removeTodo(todo.id))}
-                className="text-red-400 hover:text-red-600 transition-colors duration-200"
-                aria-label={`Remove ${todo.text}`}
-              >
-                <X className="w-5 h-5" />
-              </button>
+              <div className="space-x-2">
+                <button
+                  onClick={() => dispatch(removeTodo(todo.id))}
+                  className="text-red-400 hover:text-red-600 transition-colors duration-200"
+                  aria-label={`Remove ${todo.text}`}
+                >
+                  <X className="w-5 h-5" />
+                </button>
+                <button
+                  onClick={() => dispatch(updateTodo(todo.id))}
+                  className="text-blue-400 hover:text-blue-600 transition-colors duration-200"
+                  aria-label={`Update ${todo.text}`}
+                >
+                  <Edit className="w-5 h-5" />
+                </button>
+              </div>
             </motion.li>
           ))}
         </AnimatePresence>
