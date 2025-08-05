2266e4bcf560eab1ae06b7c8e99b34cd2e1ac8c9
elisha <elishajameel2007@gmail.com>
2025-08-05 18:42:33 +0500

    added total todos in the todo app

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import AddTodo from "./components/AddTodo";
 import Todo from "./components/Todo";
 import { motion } from "framer-motion";
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const todos = useSelector((state) => state.todos);
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-950 to-gray-900 text-gray-50 flex flex-col items-center pt-15 px-4">
       <motion.h1
@@ -21,8 +23,13 @@ const App = () => {
         transition={{ duration: 0.4, ease: "easeOut", delay: 0.2 }}
         className="w-full max-w-2xl"
       >
-        <AddTodo />
-        <Todo />
+        <div className="flex flex-col">
+          <div className="flex justify-end mb-2 mr-5 font-bold text-lg">Total Todos: {todos.length}</div>
+          <div>
+            <AddTodo />
+            <Todo />
+          </div>
+        </div>
       </motion.div>
     </div>
   );
